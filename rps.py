2e59952a4be2bc9094909ef680b8633965f4cdf5
Matthew Dulle <ps.mdulle@outlook.com>
2026-02-26 18:59:30 -0500

    updated game

--- rps.py
+++ rps.py
@@ -1,2 +1,60 @@
 # create a rock, paper, scissor game
+import random
+from colorama import init, Fore
+init(autoreset=True)
 choices = ["rock", "paper", "scissors"]
+print("Welcome to Roshambo! Can you beat me?")
+wincounter = 0
+playerscore = 0
+computerscore = 0
+while True:
+    # add a score
+    print(f"Your score: {playerscore} | Computer: {computerscore} \n"  )
+    # need to be able to take users input so they can select
+    player = input("Choose either rock, paper, or scissors:")
+    #need to give random choice to computer
+    computer = random.choice(choices)
+    print("Your opponent chooses:", computer)
+    if computer == player:
+        print("Its a Tie")
+        wincounter = 0
+    elif computer == "rock" and player == "paper":
+        print(Fore.GREEN+"You Win!")
+        wincounter += 1
+        playerscore += 1
+    elif computer == "paper" and player == "rock":
+        print("You Lose!")
+        wincounter = 0
+        computerscore += 1
+    elif computer == "rock" and player == "scissors":
+        print("You Lose!")
+        wincounter = 0
+        computerscore += 1
+    elif computer == "paper" and player == "scissors":
+        print(Fore.GREEN+"You Win!")
+        wincounter += 1
+        playerscore += 1
+    elif computer == "scissors" and player == "paper":
+        print("You Lose!")
+        wincounter = 0
+        computerscore += 1
+    elif computer == "scissors" and player == "rock":
+        print(Fore.green+"You Win!")
+        wincounter += 1
+        playerscore += 1
+    elif player == "gun":
+        print(Fore.GREEN+"Woah, woah, woah, no need to escalate things")
+        wincounter += 1
+        playerscore += 1
+    else: 
+        print("Thats not an option! Please try again")
+    print("Win Streak:", wincounter)
+    play = input("Would you like to play again (Y/N)")
+    if play == "Y":
+         print("Thanks for playing, Lests go another round!")
+            
+    elif play == "N":
+        print("Good game! Have a good one!")
+        break
+ 
+    
